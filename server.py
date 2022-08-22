d434619585bbcc390bbf708ff0bea894ca7e9d30
Abhishek <80732845+Lostman76@users.noreply.github.com>
2022-08-22 12:12:34 +0530

    Update server.py

--- server.py
+++ server.py
@@ -21,5 +21,5 @@ def classify_image():
 if __name__ == "__main__":
     print("Starting Python Flask Server For Sports Celebrity Image Classification")
     util.load_saved_artifacts()
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5100))
     app.run(host='0.0.0.0', port=port)
